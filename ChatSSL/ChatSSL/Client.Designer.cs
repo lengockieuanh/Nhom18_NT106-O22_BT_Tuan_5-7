f987f9ecd7f90a8fc5606d16cf0a37ddd4944f4a
Anh Lê Ngọc Kiều <22520047@ms.uit.edu.vn>
2024-04-13 13:46:45 +0700

    Client

--- ChatSSL/ChatSSL/Client.Designer.cs
+++ ChatSSL/ChatSSL/Client.Designer.cs
@@ -29,12 +29,146 @@ namespace ChatSSL
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIPServer = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.rtbView = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Server IP";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(417, 338);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(95, 26);
+            this.tbPort.TabIndex = 32;
+            // 
+            // tbIPServer
+            // 
+            this.tbIPServer.Location = new System.Drawing.Point(69, 340);
+            this.tbIPServer.Name = "tbIPServer";
+            this.tbIPServer.Size = new System.Drawing.Size(216, 26);
+            this.tbIPServer.TabIndex = 31;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(291, 266);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(86, 38);
+            this.btnConnect.TabIndex = 30;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(67, 271);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(218, 26);
+            this.tbUserName.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "User name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(605, 414);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(119, 45);
+            this.btnSend.TabIndex = 26;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(69, 414);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(513, 45);
+            this.tbMessage.TabIndex = 25;
+            // 
+            // rtbView
+            // 
+            this.rtbView.Location = new System.Drawing.Point(69, 11);
+            this.rtbView.Name = "rtbView";
+            this.rtbView.Size = new System.Drawing.Size(666, 229);
+            this.rtbView.TabIndex = 24;
+            this.rtbView.Text = "";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(806, 487);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIPServer);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.rtbView);
+            this.Name = "Client";
             this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIPServer;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.RichTextBox rtbView;
     }
 }
--- ChatSSL/ChatSSL/Client.cs
+++ ChatSSL/ChatSSL/Client.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,47 @@ namespace ChatSSL
 {
     public partial class Client : Form
     {
+        // Create tcp client
+        private TcpClient client;
+        // Create form server
+        private Server server = new Server();
         public Client()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            // Define IP Endpoint of server
+            IPAddress ipadd = IPAddress.Parse(tbIPServer.Text);
+            int port = Convert.ToInt32(tbPort.Text);
+            IPEndPoint ipend = new IPEndPoint(ipadd, port);
+            // Connect client to server by using IP endpoint
+            try
+            {
+                client.Connect(ipend);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+            // Create stream to write message
+            NetworkStream stream = client.GetStream();
+            string message = tbUserName.Text + ": " + tbMessage.Text;
+            rtbView.AppendText(message + "\r\n");
+            // Encode message from string to bytes
+            Byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+            // Write message into stream
+            stream.Write(sendBytes, 0, sendBytes.Length);
+            // Message textbox will be empty then
+            tbMessage.Text = "";
+            // Dispose client but the connection is still kept
+            if (client != null)
+            {
+                client.Close();
+            }
         }
     }
 }
