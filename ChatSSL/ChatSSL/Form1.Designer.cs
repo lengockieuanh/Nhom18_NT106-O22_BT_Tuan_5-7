8cb5bf1a6afe53fd90d1f4420c0598cfef96c5ff
Trandai108 <22520184@gm.uit.edu.vn>
2024-04-13 13:34:58 +0700

    Initialize Client and Server

---
+++ ChatSSL/ChatSSL/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ChatSSL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btServer = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btServer
+            // 
+            this.btServer.Location = new System.Drawing.Point(208, 94);
+            this.btServer.Name = "btServer";
+            this.btServer.Size = new System.Drawing.Size(384, 86);
+            this.btServer.TabIndex = 0;
+            this.btServer.Text = "Server";
+            this.btServer.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(384, 86);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btServer;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
